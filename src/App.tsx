deffb0af2648d4a1c6bbd433005706873f8676a5
whisper74541 <zaq4869@naver.com>
2024-01-10 00:43:17 +0900

    publishing 작업 중

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,78 @@ import './App.css';
 function App() {
   return (
     <div className="clover">
-      <h1>파이팅</h1>
+      <header className='header'>
+        <h1>January</h1>
+        <h2>2019</h2>
+      </header>
+      <div className='main-container'>
+        <div className='day'>
+          <div>SUN</div>
+          <div>MON</div>
+          <div>TUE</div>
+          <div>WED</div>
+          <div>THU</div>
+          <div>FRI</div>
+          <div>SAT</div>
+        </div>
+        <section className='cal'>
+          <article>
+            <div className='week'>
+              <div className='date'>1</div>
+              <div className='date'>2</div>
+              <div className='date'>3</div>
+              <div className='date'>4</div>
+              <div className='date'>5</div>
+              <div className='date'>6</div>
+              <div className='date'>7</div>
+            </div>
+            <div className='week'>
+              <div className='date'>1</div>
+              <div className='date'>2</div>
+              <div className='date'>3</div>
+              <div className='date'>4</div>
+              <div className='date'>5</div>
+              <div className='date'>6</div>
+              <div className='date'>7</div>
+            </div>
+            <div className='week'>
+              <div className='date'>1</div>
+              <div className='date'>2</div>
+              <div className='date'>3</div>
+              <div className='date'>4</div>
+              <div className='date'>5</div>
+              <div className='date'>6</div>
+              <div className='date'>7</div>
+            </div>
+            <div className='week'>
+              <div className='date'>1</div>
+              <div className='date'>2</div>
+              <div className='date'>3</div>
+              <div className='date'>4</div>
+              <div className='date'>5</div>
+              <div className='date'>6</div>
+              <div className='date'>7</div>
+            </div>
+          </article>
+        </section>
+        <section className='notes'>
+          <article className='choi'>
+            <div className='notes-title'>
+              <h2>NOTES</h2>
+            </div>
+            <div className='lines'>
+              <div className='line'></div>
+              <div className='line'></div>
+              <div className='line'></div>
+              <div className='line'></div>
+              <div className='line'></div>
+              <div className='line'></div>
+              <div className='line'></div>
+              <div className='line'></div>
+            </div>
+          </article>
+        </section>
+      </div>
     </div>
   );
 }
